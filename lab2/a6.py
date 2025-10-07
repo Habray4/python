dc00f764e10c46cb27a16e58f608b5111ad1fb47
Habray4 <egor.svib@gmail.com>
2025-10-07 09:20:57 +0300

    a6.py

--- lab2/a6.py
+++ lab2/a6.py
@@ -1,7 +1,14 @@
-##########################no rady
 n = int(input())
-b = int(input())
-if a%b == 0:
-    print("YES")
-else:
-    print("NO")
+h = n//3600
+n = n % 3600
+m = n//60
+s = n%60
+print(h, end="")
+print(":", end="")
+if m < 10:
+    print("0", end="")
+print(m, end="")
+print(":", end="")
+if s < 10:
+    print("0", end="")
+print(s)
