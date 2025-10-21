b86e30ac6a1667dfcff2948c330f38027c1caaad
Habray4 <egor.svib@gmail.com>
2025-10-21 12:06:02 +0300

    Update A3.py

--- lab3/A3.py
+++ lab3/A3.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+lu = int(input())
+pu = int(input())
+u = pu - lu
+if(u < 0):
+    u = u + 10000
+m = 21
+h = u
+if(h>800):
+    m = m + ((h-800) * 0.025)
+    h = 800
+if(h>600):
+    m = m + ((h-600) * 0.04)
+    h = 600
+if(h>300):
+    m = m + ((h-300) * 0.06)
+    
+print("Предыдущее")
+print("  ", lu)
+
+print("Текущее")
+print("  ", pu)
+
+print("Использовано")
+print("  ", u)
+
+print("К оплате")
+print("  ", round(m, 2))
+
+print("Ср. цена за m^3")
+print("  ", round(m/u, 2))
