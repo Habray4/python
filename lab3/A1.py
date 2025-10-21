550e3980fff1e6415a5180ef6e591c02333c7b39
Habray4 <egor.svib@gmail.com>
2025-10-21 10:51:37 +0300

    Update A1.py

--- lab3/A1.py
+++ lab3/A1.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+print("x1   y1")
+x1 = float(input())
+y1 = float(input())
+print("x2   y2")
+x2 = float(input())
+y2 = float(input())
+
+if(x1>0 and y1>0 and x2>0 and y2>0):
+    print("Yes, I")
+elif(x1<0 and y1>0 and x2<0 and y2>0):
+    print("Yes, II")
+elif(x1<0 and y1<0 and x2<0 and y2<0):
+    print("Yes, III")
+elif(x1>0 and y1<0 and x2>0 and y2<0):
+    print("Yes, IV")
+else:
+    print("No")
